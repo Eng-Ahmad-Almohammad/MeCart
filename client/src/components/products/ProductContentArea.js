09fce99642f37970aa3a8b784370378725a305b8
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2021-08-10 18:23:08 +0300

    solve a conflict

--- client/src/components/products/ProductContentArea.js
+++ client/src/components/products/ProductContentArea.js
@@ -66,18 +66,7 @@ class ProductContentArea extends Component {
         </Row>
       </div>
     ):(
-<<<<<<< HEAD
       <ProductInstance handler={this.handleModal}  item={this.props.item}/>
-=======
-      <div>
-        <Button onClick={()=>
-        this.setState({ isModalOpen: !this.state.isModalOpen })
-        }>
-          close
-        </Button>
-
-      </div>
->>>>>>> d18a5da9 (recent changes)
     )
   }
 }
