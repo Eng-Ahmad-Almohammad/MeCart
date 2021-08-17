a44801af79c43e28f2acd68a0d64c8cbe5aad564
Hadeaji <hadeaji@gmail.com>
2021-08-17 08:14:43 +0300

    small fixes

--- client/src/components/leaderboard/LeaderBoardContentArea.js
+++ client/src/components/leaderboard/LeaderBoardContentArea.js
@@ -1,6 +1,6 @@
-import {connect} from "react-redux";
-import React, {Component} from "react";
-import {fetchLeaderboard} from '../../actions'
+import { connect } from "react-redux";
+import React, { Component } from "react";
+import { fetchLeaderboard } from '../../actions'
 import Leaderboard from 'react-leaderboard';
 
 class LeaderBoardContentArea extends Component {
@@ -9,37 +9,37 @@ class LeaderBoardContentArea extends Component {
         this.state = false
     }
 
-    
-    componentDidMount(){
-        this.props.getData().then(res =>{
-       console.log('Usssssssssers',this.props.leaderBorder)
-       let data = {
-           users:[],
-           paginate:1
-       }
-       this.props.leaderBorder.forEach(val =>{
-           data.users.push({name:`${val.firstName} ${val.lastName}`,score:val.points})
-       })
-       this.setState(data)
-        })
-      }
 
-      
+    componentDidMount() {
+        this.props.getData().then(res => {
+            console.log('Usssssssssers', this.props.leaderBorder)
+            let data = {
+                users: [],
+                paginate: 10
+            }
+            this.props.leaderBorder.forEach(val => {
+                data.users.push({ name: `${val.firstName} ${val.lastName}`, score: val.points })
+            })
+            this.setState(data)
+        })
+    }
+
+
 
 
     render() {
-        console.log('Rendeeeer',this.state.users)
-        if (this.state){
+        console.log('Rendeeeer', this.state.users)
+        if (this.state) {
+            return (
+                <div className="content-area">
+                    <Leaderboard users={this.state.users} paginate={this.state.paginate} />
+                </div>
+            );
+        }
         return (
             <div className="content-area">
-                <Leaderboard users={this.state.users} paginate={this.state.paginate}/>
+                Wait a minute
             </div>
-        );
-        }
-        return(
-            <div className="content-area">
-            Wait a minute
-        </div>
         )
     }
 }
@@ -49,7 +49,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    getData:  () =>  dispatch(fetchLeaderboard()),
+    getData: () => dispatch(fetchLeaderboard()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LeaderBoardContentArea);
--- client/src/components/products/ProductContentArea.js
+++ client/src/components/products/ProductContentArea.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Button, Container, Row } from "react-materialize";
 import CardItem from "../CardItem";
 import { connect } from "react-redux";
-import { ContentAreaTypes, getAllProducts} from "../../actions";
+import { ContentAreaTypes, getAllProducts } from "../../actions";
 import NewProductModal from "./modals/NewProductModal";
 import ProductInstance from "./instances/productInstancesArea";
 
@@ -13,78 +13,82 @@ class ProductContentArea extends Component {
 
     this.state = {
       isModalOpen: false,
-      cards:this.props.storeList,
-      ProductIn:{},
-     
+      cards: this.props.storeList,
+      ProductIn: {},
+
     };
   }
 
-  
 
-  handleModal=()=>{
+
+  handleModal = () => {
     this.setState({ isModalOpen: !this.state.isModalOpen })
   }
-  componentDidMount(){
-    this.props.getData().then(res =>{
-    this.setState({ cards: this.props.storeList })
+  componentDidMount() {
+    this.props.getData().then(res => {
+      this.setState({ cards: this.props.storeList })
     })
   }
 
 
   render() {
-    return !this.state.isModalOpen?(
+    return !this.state.isModalOpen ? (
       <div>
         <Row>
           <Button
             className="modal-trigger"
             href="#newProductModal"
             node="button"
-            style={{marginLeft: "40px"}}
+            style={{ marginLeft: "40px" }}
           >
             New Product
           </Button>
-          <NewProductModal/>
+          <NewProductModal />
         </Row>
-        <Row>
+        <Row
+          style={{
+            overflow: 'hidden',
+          }}
+        >
           <Container className="content-area">
             {this.state.cards.map((value) => {
 
-              return  (
+              return (
                 <div>
-                <CardItem
-                  type={ContentAreaTypes.PRODUCTS} 
-                  imageUrl={value.imageUrl}
-                  hasMenu={true}
-                  listItem={value}
-                  handler={this.handleModal}
-                  title={value.name}
-                  itemDescription={value.descriptionOne}
+                  <CardItem
+                    type={ContentAreaTypes.PRODUCTS}
+                    imageUrl={value.imageUrl}
+                    hasMenu={true}
+                    listItem={value}
+                    handler={this.handleModal}
+                    title={value.name}
+                    itemDescription={value.descriptionOne}
                   // onClick={() =>
                   //   this.setState({ isModalOpen: !this.state.isModalOpen })
                   // }
-                />
+                  />
                 </div>
               )
             })}
           </Container>
         </Row>
       </div>
-    ):(
-      <ProductInstance handler={this.handleModal}  item={this.props.item}/>
+    ) : (
+      <ProductInstance handler={this.handleModal} item={this.props.item} />
     )
   }
 }
 
 const mapStateToProps = (state => {
   return {
-  storeList: state.product.product,
-  item:state.product.item
+    storeList: state.product.product,
+    item: state.product.item
   }
 
 }
 )
 const mapDispatchToProp = (dispatch) => ({
-  getData :  () =>  dispatch(getAllProducts()),
-  
+  getData: () => dispatch(getAllProducts()),
+
 })
-export default connect(mapStateToProps,mapDispatchToProp)(ProductContentArea);
+export default connect(mapStateToProps, mapDispatchToProp)(ProductContentArea);
