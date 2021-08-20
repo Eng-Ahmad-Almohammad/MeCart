716f887d825b82debd36f96a960f7170d5bac979
Hadeaji <hadeaji@gmail.com>
2021-08-20 19:01:53 +0300

    styling some cards

--- client/src/components/shopping/ShoppingContentArea.js
+++ client/src/components/shopping/ShoppingContentArea.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { chain, random } from "lodash";
 import { Button, Container, Row, Col, CardTitle, Card, Icon } from "react-materialize";
-import CardItem from "../CardItem";
+import CardItem from "./card/index";
 import { connect } from "react-redux";
 import { ContentAreaTypes, getAllShoppingList } from "../../actions";
 import NewListModal from "./modals/NewListModal";
@@ -12,82 +12,98 @@ class ShoppingContentArea extends Component {
 
     this.state = {
       isModalOpen: false,
-      cards:this.props.shoppingList,
-      shoppingList:{},
-     
+      cards: this.props.shoppingList,
+      shoppingList: {},
+
       // cards: Array(12)
       //   .fill("")
       //   .map((value, index, array) => {}),
     };
   }
 
-  componentDidMount = ()=>{
+  componentDidMount = () => {
 
-    this.props.fetchShoppingList().then(res =>{
-      console.log('from shopping list ======?',this.props.shoppingList)
+    this.props.fetchShoppingList().then(res => {
+      console.log('from shopping list ======?', this.props.shoppingList)
       this.setState({ cards: this.props.shoppingList });
     })
   }
 
-  componentDidUpdate = (prevProps) =>{
-    if (prevProps.shoppingList.length !== this.props.shoppingList.length){
-    console.log(prevProps.shoppingList)
-     console.log( this.props.shoppingList)
-     
-      this.props.fetchShoppingList().then(res =>{
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.shoppingList.length !== this.props.shoppingList.length) {
+      console.log(prevProps.shoppingList)
+      console.log(this.props.shoppingList)
+
+      this.props.fetchShoppingList().then(res => {
         this.setState({ cards: this.props.shoppingList })
-        })
+      })
     }
   }
- 
 
-  handlerShopingList = () =>{
-    this.setState({ isModalOpen: !this.state.isModalOpen,shoppingList:this.props.shoppingList.shoppingList });
+
+  handlerShopingList = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen, shoppingList: this.props.shoppingList.shoppingList });
   }
 
 
   render() {
-     console.log("frommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm",this.state.shoppingList)
-    return !this.state.isModalOpen?(
+    console.log("frommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm", this.state.shoppingList)
+    return !this.state.isModalOpen ? (
       <div>
         <Row>
-          <Button className="modal-trigger" href="#newListModal" node="button" style={{marginLeft: "40px"}}>
+          <Button className="modal-trigger" href="#newListModal" node="button" style={{ marginLeft: "40px" }}>
             New List
           </Button>
-          <NewListModal/>
+          <NewListModal />
         </Row>
         <Row>
           <Container className="content-area">
             {this.props.shoppingList.items.map((value) => {
+              console.log('my man', value);
               return (
                 <div>
-                <CardItem
-                  type={ContentAreaTypes.SHOPPING}
-                  hasMenu={true}
-                  listItem={value}
-                  handler = {this.handlerShopingList}
-                  imageUrl={`https://picsum.photos/${random(200, 300)}/${random(
-                    250,
-                    350
-                  )}`}
-                  onClick={() =>
-                    this.setState({ isModalOpen: !this.state.isModalOpen })
-                  }
-                />
+                  {/* <CardItem
+                    type={ContentAreaTypes.SHOPPING}
+                    hasMenu={true}
+                    listItem={value}
+                    handler={this.handlerShopingList}
+                    imageUrl={`https://picsum.photos/${random(200, 300)}/${random(
+                      250,
+                      350
+                    )}`}
+                    onClick={() =>
+                      this.setState({ isModalOpen: !this.state.isModalOpen })
+                    }
+                  /> */}
+                  <CardItem
+                    type={ContentAreaTypes.SHOPPING}
+                    hasMenu={true}
+                    listItem={value}
+                    handler={this.handlerShopingList}
+                    header={value.imageUrl}
+                    title={value.name || ""}
+                    itemDescription={value.description}
+                    itemsNu={value.products.length}
+                    className="hoverable"
+                    onClick={() =>
+                      this.setState({ isModalOpen: !this.state.isModalOpen })
+                    }
+                  />
+
                 </div>
               );
             })}
           </Container>
         </Row>
       </div>
-    ):(
+    ) : (
       <div>
         <Row>
-        <Button onClick={()=>
-        this.setState({ isModalOpen: !this.state.isModalOpen })
-        }>
-          close
-        </Button>
+          <Button onClick={() =>
+            this.setState({ isModalOpen: !this.state.isModalOpen })
+          }>
+            close
+          </Button>
         </Row>
         <Row>
           <Container>
@@ -95,59 +111,59 @@ class ShoppingContentArea extends Component {
               <p>list name {this.state.shoppingList.shoppingList.name}</p>
               <p>list description {this.state.shoppingList.shoppingList.description}</p>
               <Row>
-                    <Container className="content-area">
-                        {
-                             this.state.shoppingList.productInstances.map((item,indx) => {
+                <Container className="content-area">
+                  {
+                    this.state.shoppingList.productInstances.map((item, indx) => {
 
-                                return (
-                                    <Row>
-                                        <Col
-                                            m={12}
-                                            s={12}
-                                        >
-                                            <Card
-                                                closeIcon={<Icon>close</Icon>}
-                                                header={<CardTitle image="https://materializecss.com/images/sample-1.jpg" reveal waves="light" />}
-                                                reveal={
-                                                    <>
-                                                        <p>priceBeforeTax {item.priceBeforeTax}</p>
-                                                        <p>priceAfterTax {item.priceAfterTax}</p>
-                                                        <p>unitOfMeasure {item.unitOfMeasure}</p>
-                                                        <p>measurement {item.measurement}</p>
-                                                        <p>quantity {this.state.shoppingList.shoppingProducts[indx].quantity}</p>
+                      return (
+                        <Row>
+                          <Col
+                            m={12}
+                            s={12}
+                          >
+                            <Card
+                              closeIcon={<Icon>close</Icon>}
+                              header={<CardTitle image="https://materializecss.com/images/sample-1.jpg" reveal waves="light" />}
+                              reveal={
+                                <>
+                                  <p>priceBeforeTax {item.priceBeforeTax}</p>
+                                  <p>priceAfterTax {item.priceAfterTax}</p>
+                                  <p>unitOfMeasure {item.unitOfMeasure}</p>
+                                  <p>measurement {item.measurement}</p>
+                                  <p>quantity {this.state.shoppingList.shoppingProducts[indx].quantity}</p>
 
-                                                        <Button
-                                                                                                                       
-                                                           onClick={()=>this.delete(item._id, this.props.id)}
-                                                            style={{
-                                                                display: "inline-block",
-                                                                textAlign: "center",
-                                                                fontSize: "10px"
-                                                            }}
-                                                        >
-                                                            Delete
-                                                        </Button>
-                                                    </>
-                                                }
-                                                revealIcon={<Icon>more_vert</Icon>}
-                                                title="Card Title"
-                                            >
-                                                <p>
+                                  <Button
 
-                                                </p>
-                                            </Card>
-                                        </Col>
-                                    </Row>
-                                )
-                            })
-                        }
-                    </Container>
-                </Row>
-             
+                                    onClick={() => this.delete(item._id, this.props.id)}
+                                    style={{
+                                      display: "inline-block",
+                                      textAlign: "center",
+                                      fontSize: "10px"
+                                    }}
+                                  >
+                                    Delete
+                                  </Button>
+                                </>
+                              }
+                              revealIcon={<Icon>more_vert</Icon>}
+                              title="Card Title"
+                            >
+                              <p>
+
+                              </p>
+                            </Card>
+                          </Col>
+                        </Row>
+                      )
+                    })
+                  }
+                </Container>
+              </Row>
+
             </div>
           </Container>
         </Row>
-        
+
       </div>
     )
   }
@@ -158,9 +174,9 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchShoppingList:  () => dispatch( getAllShoppingList()),
-  
- 
+  fetchShoppingList: () => dispatch(getAllShoppingList()),
+
+
 });
 
-export default connect(mapStateToProps,mapDispatchToProps)(ShoppingContentArea);
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingContentArea);
--- client/src/components/supermarkets/SupermarketContentArea.js
+++ client/src/components/supermarkets/SupermarketContentArea.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
 
-import { Button, Container, Row } from "react-materialize";
-import CardItem from "../CardItem";
+import { Button, Container, Row, Card, Col, Icon } from "react-materialize";
+import CardItem from "./card/index";
 import { connect } from "react-redux";
-import { ContentAreaTypes,getAllSupermarkets, getSupermarket } from "../../actions";
+import { ContentAreaTypes, getAllSupermarkets, getSupermarket } from "../../actions";
 import NewStoreModal from "./modals/NewStoreModal";
+import CardItemHeader from "../CardItem/CardItemHeader";
 
 class SupermarketContentArea extends Component {
   constructor(props) {
@@ -13,43 +14,43 @@ class SupermarketContentArea extends Component {
     this.state = {
       isModalOpen: false,
       cards: this.props.supermarketList,
-      change:false,
+      change: false,
     };
   }
-  componentDidMount(){
-    this.props.getData().then(res =>{
+  componentDidMount() {
+    this.props.getData().then(res => {
       // console.log('Hellllllllllllo',this.props.storeList)
-    this.setState({ cards: this.props.supermarketList })
+      this.setState({ cards: this.props.supermarketList })
     })
 
-    
+
 
   }
-   
-  componentDidUpdate = (prevProps) =>{
-    if (prevProps.supermarketList.length !== this.props.supermarketList.length){
-    console.log(prevProps.supermarketList)
-     console.log( this.props.supermarketList)
-     
-      this.props.getData().then(res =>{
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.supermarketList.length !== this.props.supermarketList.length) {
+      console.log(prevProps.supermarketList)
+      console.log(this.props.supermarketList)
+
+      this.props.getData().then(res => {
         this.setState({ cards: this.props.supermarketList })
-        })
+      })
     }
   }
 
- 
 
-  handlerStoresList = () =>{
+
+  handlerStoresList = () => {
     this.setState({ isModalOpen: !this.state.isModalOpen });
-    }
+  }
 
 
 
   render() {
-    return !this.state.isModalOpen?(
+    return !this.state.isModalOpen ? (
       <div>
         <Row>
-          <Button className="modal-trigger" href="#newStoreModal" node="button" style={{marginLeft: "40px"}}>
+          <Button className="modal-trigger" href="#newStoreModal" node="button" style={{ marginLeft: "40px" }}>
             New Store
           </Button>
           <NewStoreModal handler={this.handler} />
@@ -57,28 +58,76 @@ class SupermarketContentArea extends Component {
         <Row>
           <Container className="content-area">
             {this.state.cards.map((value) => {
+              console.log('dsad', value);
               return (
                 <div>
-                <CardItem
-                  type={ContentAreaTypes.SUPERMARKETS}
-                  imageUrl={value.imageUrl}
-                  hasMenu={true}
-                  listItem={value}
-                  handler = {this.handlerStoresList}
-                  onClick={() =>
-                    this.setState({ isModalOpen: !this.state.isModalOpen })
-                  }
-                />
+                  {/* <CardItem
+                    type={ContentAreaTypes.SUPERMARKETS}
+                    imageUrl={value.imageUrl}
+                    hasMenu={true}
+                    listItem={value}
+                    className="hoverable"
+                    handler={this.handlerStoresList}
+                    onClick={() =>
+                      this.setState({ isModalOpen: !this.state.isModalOpen })
+                    }
+                  /> */}
+                  <CardItem
+                    type={ContentAreaTypes.SUPERMARKETS}
+                    hasMenu={true}
+                    listItem={value}
+                    address={value.address}
+                    handler={this.handlerStoresList}
+                    header={<CardItemHeader imageUrl={value.imageUrl} />}
+                    title={value.name || ""}
+                    className="hoverable"
+                    itemDescription={value.businessInfo}
+                    onClick={() =>
+                      this.setState({ isModalOpen: !this.state.isModalOpen })
+                    }
+
+                  >
+                    <div
+                      style={{
+                        display: 'flex',
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          width: '60%',
+                        }}
+                      >
+                        <Col s={6} className="left-align"
+                          style={{
+                            width: '100%',
+                          }}
+                        >
+                          <span className="star-rating">
+                            {value.address}
+                          </span>
+                        </Col>
+
+                      </div>
+
+                    </div>
+                    <p
+                      style={{
+                        margin: '2px'
+                      }}
+                    >{value.businessInfo}</p>
+                  </CardItem>
                 </div>
               );
             })}
           </Container>
         </Row>
       </div>
-    ):(
+    ) : (
       <div>
-        <Button onClick={()=>
-        this.setState({ isModalOpen: !this.state.isModalOpen })
+        <Button onClick={() =>
+          this.setState({ isModalOpen: !this.state.isModalOpen })
         }>
           close
         </Button>
@@ -93,7 +142,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProp = (dispatch) => ({
-  getData :  () =>  dispatch(getAllSupermarkets()),
-  getItem:(id) => dispatch(getSupermarket(id))
+  getData: () => dispatch(getAllSupermarkets()),
+  getItem: (id) => dispatch(getSupermarket(id))
 })
-export default connect(mapStateToProps,mapDispatchToProp)(SupermarketContentArea);
+export default connect(mapStateToProps, mapDispatchToProp)(SupermarketContentArea);
